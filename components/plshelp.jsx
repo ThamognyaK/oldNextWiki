12802a4ed27c8ac8b12faaa63be102bd21e9d390
Thamognya Kodi <101309160+ThamognyaK@users.noreply.github.com>
2023-10-11 19:00:11 +0000

    Update 5 files

--- components/plshelp.jsx
+++ components/plshelp.jsx
@@ -14,9 +14,22 @@ const Bgimage = () => {
         className="absolute inset-0 bg-cover bg-center z-0"
         style={{
           backgroundImage:
-            'url("https://static.igem.wiki/teams/4629/wiki/igem-quote.png")',
+            'url("https://static.igem.wiki/teams/4629/wiki/quotebackground.png")',
         }}
       ></div>
+
+<div className="absolute inset-0 flex items-center text-center justify-center z-100  drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)]">
+
+<div class="flex items-center justify-center h-screen text-white">
+    <div class="text-center">
+        <h1 class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold mb-4">The difference between life and death:</h1>
+        <h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold mb-4">48 hours</h1>
+        <h1 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold">one gene.</h1>
+    </div>
+</div>
+
+
+      </div>
 
     </div>
   );
--- components/whateverthisis.jsx
+++ components/whateverthisis.jsx
@@ -12,22 +12,24 @@ const Pls = () => {
         }}
       ></div>
 
-      <div class="grid grid-cols-2 gap-5 border-transparent absolute inset-0 text-center items-center justify-center z-100">
-        <div class="p-5"></div>
-        <div class="p-5">
-          <h1 className="text-what text-5xl font-black">INCREASE</h1>
-          <h1 className="text-xl font-bold">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 border-transparent absolute inset-0 text-center items-center justify-center z-100">
+        <div className="p-5"></div>
+        <div className="p-5">
+          <h1 className="text-what text-3xl sm:text-5xl font-black">
+            INCREASE
+          </h1>
+          <h1 className="text-xl sm:text-2xl font-bold">
             plant stress response capabilities.
           </h1>
         </div>
-        <div class="p-5">
-          <h1 className="text-whatt text-5xl font-black">35%</h1>
-          <h1 className="text-xl font-bold">
+        <div className="p-5">
+          <h1 className="text-whatt text-3xl sm:text-5xl font-black">35%</h1>
+          <h1 className="text-xl sm:text-2xl font-bold">
             of farmers in Chantaburi cited droughts as the most pressing issue
             towards their crop yields.
           </h1>
         </div>
-        <div class="p5"></div>
+        <div className="p-5"></div>
       </div>
     </div>
   );
