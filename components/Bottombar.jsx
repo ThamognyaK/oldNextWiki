48e75f9b2997c9b07c5b8d74d7715790cc81c363
Thamognya Kodi <101309160+ThamognyaK@users.noreply.github.com>
2023-10-11 14:58:36 +0000

    Update 3 files

--- components/Bottombar.jsx
+++ components/Bottombar.jsx
@@ -1,8 +1,58 @@
-const Bottombar = () => {
-  return (
-    <div class="ellipse">
-    </div>
-  );
-};
+import React from "react";
 
-export default Bottombar;
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="sponsor-section">
+        {/* Sponsor images go here */}
+        <img
+          src="https://static.igem.wiki/teams/4629/wiki/chulalongkorn-university-official-logo-svg.png"
+          className="sponsor-image"
+        />
+        <img
+          src="https://static.igem.wiki/teams/4629/wiki/bangchak.png"
+          className="sponsor-image"
+        />
+        <img
+          src="https://static.igem.wiki/teams/4629/wiki/logo-mahachok-fruits-01.png"
+          className="sponsor-image"
+        />
+
+        <img
+          src="https://static.igem.wiki/teams/4629/wiki/ris.png"
+          className="sponsor-image"
+        />
+
+        {/* Add more sponsor images as needed */}
+      </div>
+      <div className="cc-license">
+        {/* Creative Commons license information */}
+
+        <p class="mb-0">
+          <small>
+            &copy; 2023 - Content on this site is licensed under a{" "}
+            <a
+              class="subfoot"
+              href="https://creativecommons.org/licenses/by/4.0/"
+              rel="license"
+            >
+              Creative Commons Attribution 4.0 International license
+            </a>
+            .
+          </small>
+        </p>
+        <p>
+          <small>
+            The repository used to create this website is available at{" "}
+            <a href="https://gitlab.igem.org/2023/thailand-ris">
+              gitlab.igem.org/2023/thailand-ris
+            </a>
+            .
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
