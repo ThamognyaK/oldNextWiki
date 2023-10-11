b141392d31fab2d2cef8207c58468bcacbe12115
Kaka123 <dhiratht25@rism.ac.th>
2023-10-11 14:15:37 +0700

    fixed gitlab ci

--- pages/design.js
+++ pages/design.js
@@ -77,6 +77,32 @@ function DesignPage() {
                         <br/>
                         &nbsp;&nbsp;&nbsp;&nbsp;Having the FASTA sequences of the chloroplastic DzGCL and the mitochondrial mDzGCL, the first thing done was acquiring predictions of their maps through the HMMER program <sup>7</sup>, HMMScan, which utilizes the hidden Markov model and statistical analyses to find matches in the sequence with known functional domain sequences in a database. In this case, the Pfam protein database<sup>8</sup> was used. The maps outputted by HMMScan of DzGCL and mDzGCL are displayed in Fig. 4 and Fig. 5, respectively. Both figures show that HMMScan was able to identify the signature glutamate-cysteine ligase family 2 functional domain of a GCL represented by the brown section in the maps. Furthermore, the length of the functional domains calculated by finding the difference in the end and the start amino acid positions were found to be the same. Thus, the modifications in the mDzGCL were shown to have not disturbed the functional domain of GCL. 
                     </p>
+                    <img class="figure"src="https://static.igem.wiki/teams/4629/wiki/dry-lab/fig4.jpg"/>
+                        <h5>
+                            <b>Figure 4.</b> Map of DzGCL (unmodified) predicted through HMMScan (HMMer) showing the signature glutamate-cysteine ligase family 2 functional domain of a GCL. 
+                        </h5>
+                    <img class="figure"src="https://static.igem.wiki/teams/4629/wiki/dry-lab/fig5.jpg"/>
+                        <h5>
+                            <b>Figure 5.</b> Map of mDzGCL (modified )predicted through HMMScan (HMMer), which also shows the signature glutamate-cysteine ligase family 2 functional domain of a GCL. 
+                        </h5>
+                    <p>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+To further verify the integrity of the enzymatic function of GCL in mDzGCL, 3D structures of DzGCL and mDzGCL were predicted using AlphaFold2, a software that combines empirical knowledge on protein structure with a deep-learning algorithm, leveraging evolutionary data through multiple-sequence alignment. This approach often yields protein models of comparable accuracy to experimentally determined structures<sup>9, 10</sup>. The predictions obtained from AlphaFold2 were then visualized in PyMOL v2.5.5 (by Schrödinger) for analysis. Shown in Fig. 6 and Fig. 7, the 3D structures of DzGCL and mDzGCL, respectively, are observably similar except for the tail, in which the difference is due to modifications in the N-terminal when replacing the chloroplast transit peptide in mDzGCL with mitochondrial one. The similarity between the two 3D structures is better displayed in Fig. 8, where the DzGCL (green) and mDzGCL (red) are overlaid, and a complete overlap is observable. Therefore, since the structure of the modified GCL (mDzGCL) closely resembles that of the unmodified GCL (DzGCL), the enzymatic function should also remain approximately the same. 
+
+                    </p>
+                    <img class="figure"src="https://static.igem.wiki/teams/4629/wiki/dry-lab/fig6.jpg"/>
+                        <h5>
+                            <b>Figure 4.</b> Map of DzGCL (unmodified) predicted through HMMScan (HMMer) showing the signature glutamate-cysteine ligase family 2 functional domain of a GCL. 
+                        </h5>
+                    <img class="figure"src="https://static.igem.wiki/teams/4629/wiki/dry-lab/fig7.jpg"/>
+                        <h5>
+                            <b>Figure 5.</b> Map of mDzGCL (modified )predicted through HMMScan (HMMer), which also shows the signature glutamate-cysteine ligase family 2 functional domain of a GCL. 
+                        </h5>
+                    <img class="figure"src="https://static.igem.wiki/teams/4629/wiki/dry-lab/fig8.jpg"/>
+                        <h5>
+                            <b>Figure 4.</b> Map of DzGCL (unmodified) predicted through HMMScan (HMMer) showing the signature glutamate-cysteine ligase family 2 functional domain of a GCL. 
+                        </h5>
+                   
                     
                 <h1 id="header3">Plasmid Design</h1>
                 
